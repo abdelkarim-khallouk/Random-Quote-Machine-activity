7518d5ed573323a2cb5dc065b5959ce82f0d9a78
karimkhl <khlgeek@gmail.com>
2018-04-01 02:08:39 +0100

    A Random Quote Machine
    This small application:
    - Use API from forismatoc.com to get data.
    - Can allow you to show a new random quote by one button click.
    - Let you to tweet out your favorite quote.

---
+++ README.md
@@ -0,0 +1,6 @@
+A Random Quote Machine
+
+This small application:
+- Use API from forismatoc.com to get data.
+- Can allow you to show a new random quote by one button click.
+- Let you to tweet out your favorite quote.
---
+++ main.js
@@ -0,0 +1,48 @@
+    var quote;
+    var author;
+      
+    function getNewQuote(){
+        
+        $.ajax( {
+          url: 'http://api.forismatic.com/api/1.0/',
+          jsonp: 'jsonp',
+          dataType: 'jsonp',
+          data: {
+              method: 'getQuote',
+              lang: 'en',
+              format: 'jsonp'
+          },
+            
+          success: function(data) {
+            quote = data.quoteText;
+            author = data.quoteAuthor;
+            
+            if(quote){
+                $(".qod-text").html("&ldquo;" + quote + "	&rdquo;");
+            }
+            if(author){
+                $(".qod-author").html("&mdash;" + author);
+            }
+            else{
+                $(".qod-author").text("&mdash;" + unknown);
+              }
+          },
+          cache: false
+        });
+    }
+
+  $(document).ready(function() {
+      
+    $("#getQuote").on('click', function(e) {
+        e.preventDefault();
+        getNewQuote();
+    });
+      
+    $("#shareQuote").on('click', function(e) {
+        e.preventDefault();
+        
+        var text = quote + " -- " + author;
+        window.open('http://twitter.com/share?text='+encodeURIComponent(text));
+    });
+      
+});
